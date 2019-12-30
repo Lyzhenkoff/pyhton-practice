8a2732e1fc6ed22db9359564bc9139165e48f6ab
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2019-12-30 13:51:53 +0300

    update

---
+++ task-2/task2.py
@@ -0,0 +1,4 @@
+a = input('Whats your name ');
+b = input('How old are you ');
+
+print('Hi!My name is', a ,'And Im', b ,'years old')
---
+++ task-3/task1.py
@@ -0,0 +1,5 @@
+a = [1,3,5,5];
+
+for elem in a:
+    if elem == 3:
+        print(elem)
