bb37121e9b1fa2ebad22a3102ba2363bd6c6a511
kirilllyzhenkoff <kirilllyzhenkoff@gmail.com>
2020-01-10 10:39:45 +0300

    update

---
+++ task-10/task1.py
@@ -0,0 +1,14 @@
+a = int(input('Введите число'))
+b = int(input('Введите число'))
+spo = input('Введите операцию(+,-,*,/)')
+
+ if spo = '+':
+    c = a + b
+elif spo = '-':
+    c = a - b
+elif spo == '*':
+    q = a * b
+elif spo == '/':
+    f = a/b
+
+print(a,b,spo)
